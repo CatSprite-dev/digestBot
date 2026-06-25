0f5fd92e8d5fbe6c26dbcce0f921c85349e39e58
CatSprite-dev <catspriteee@gmail.com>
2026-06-25 22:55:32 +0300

    feat: add domain models

---
+++ internal/model/model.go
@@ -0,0 +1,24 @@
+package model
+
+import "time"
+
+type Chat struct {
+	ID       int64
+	UserName string
+	Title    string
+	AddedAt  time.Time
+}
+
+type Message struct {
+	ID     int64
+	ChatID int64
+	Sender string
+	Text   string
+	SentAt time.Time
+}
+
+type DigestCursor struct {
+	UserID     int64
+	ChatID     int64
+	LastReadAt time.Time
+}
